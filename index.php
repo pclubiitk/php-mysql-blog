4dacbbf0a8f5b4553c481520c243796b0dc74a19
Shivanshu Agrawal <shivanshuag@gmail.com>
2014-02-07 13:50:24 +0530

    index.php with only html

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Poster</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="wrap">
+		<form action="insert.php" name="postForm" method="post">
+				<textarea name="post" id="post" rows="2" cols="60"></textarea>
+			<input type="submit" value="POST">
+		</form>
+	</div>
+
+</body>
+
+</html>
